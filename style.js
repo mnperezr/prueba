2b1507991376ef6eb3bbfafa506ac51e57bced0a
mnperezr <nelly.perez@blinklearning.com>
2017-10-10 09:59:08 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,31 @@
 
+(function (blink) {
+	'use strict';
+
+	var cem_jorgecalvo = function () {
+			blink.theme.styles.basic.apply(this, arguments);
+		},
+		page = blink.currentPage;
+
+	cem_jorgecalvo.prototype = {
+		bodyClassName: 'content_type_clase_cem_jorgecalvo',
+		extraPlugins: ['image2'],
+		ckEditorStyles: {
+			name: 'cem_jorgecalvo',
+			styles: [
+
+				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title1'} },
+				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title2'} },
+				{ name: 'Título 3', element: 'h4', attributes: { 'class': 'bck-title3'} },
+
+				{ name: 'Énfasis', element: 'span', attributes: { 'class': 'bck-enfasis' }},
+				{ name: 'Enunciado actividad', element: 'h4', attributes: { 'class': 'bck-title-activity' }},
+
+				{ name: 'Tabla centrada', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-center'} },
+				{ name: 'Celda encabezado', element: 'td', attributes: { 'class': 'bck-td' } },
+
+				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-1' } },
+				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-2' } },
+				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-3' } }
+			]
+		}
